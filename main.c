2bf42cad562c1407e32b5462be9990f1eeebe720
rjmarin <rjmarin@miuandes.cl>
2018-10-18 01:39:55 -0300

    primeros intentos

--- main.c
+++ main.c
@@ -15,22 +15,32 @@ how to use the page table and disk interfaces.
 #include <string.h>
 #include <errno.h>
 
-void page_fault_handler( struct page_table *pt, int page )
+
+void page_fault_handler( struct page_table *pt, int page)
 {
 	printf("page fault on page #%d\n",page);
-	exit(1);
-}
+	exit(0);
+
+	}
+	
 
 int main( int argc, char *argv[] )
 {
 	if(argc!=5) {
 		/* Add 'random' replacement algorithm if the size of your group is 3 */
-		printf("use: virtmem <npages> <nframes> <lru|fifo> <sort|scan|focus>\n");
+		printf("use: virtmem <npages> <nframes> <lru|fifo|custom> <sort|scan|focus>\n");
 		return 1;
 	}
 
 	int npages = atoi(argv[1]);
 	int nframes = atoi(argv[2]);
+	int  marcos_table_libres[nframes];
+	int  marcos_table[nframes];
+	for (int i = 0; i < nframes; ++i)
+	{
+		marcos_table[i] = i;
+	}
+	const char * algorithm = argv[3];
 	const char *program = argv[4];
 
 	struct disk *disk = disk_open("myvirtualdisk",npages);
@@ -40,7 +50,9 @@ int main( int argc, char *argv[] )
 	}
 
 
+
 	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
+
 	if(!pt) {
 		fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
 		return 1;
@@ -49,20 +61,91 @@ int main( int argc, char *argv[] )
 	char *virtmem = page_table_get_virtmem(pt);
 
 	char *physmem = page_table_get_physmem(pt);
+	int  *frame;
+	int  * bits;
+	for (int paginas = 0; paginas < npages ; ++paginas)
+	{
+		if(!strcmp(algorithm,"lru")) {
+			//implementar lru
+			
+		int a_disco = 0;
+		if (npages > nframes)
+		{
+			int i = 0;
+			while(marcos_table_libres[i]==-1 && i <= marcos_table[i])
+			{
 
-	if(!strcmp(program,"sort")) {
-		sort_program(virtmem,npages*PAGE_SIZE);
+				if (i == nframes)
+				{
+					a_disco = 1;
+					break;
 
-	} else if(!strcmp(program,"scan")) {
+				}
+				i++;
+			}
+			if (a_disco== 0 && marcos_table_libres[i] != -1  && i <= marcos_table[i] )
+			{
+				printf(" holaa %d\n",marcos_table_libres[i]  );
+				page_table_get_entry(pt,paginas, frame, bits);
+				int frames = frame;
+				int bit  = bits;
+				page_table_set_entry(pt, paginas, frames,bit);
+				marcos_table_libres[i] = -1;
+				printf(" holaa %d\n",marcos_table_libres[i]  );
+
+
+			}else{
+				printf(" hola como %d\n",marcos_table_libres[i]  );
+				//manejar disco
+			}
+
+		}
+		
+	}
+	if(npages ==  nframes){
+		for (int i = 0; i < nframes; ++i)
+		{
+			page_table_set_entry(pt, i, i,PROT_WRITE);
+		}
+
+		
+
+} else if(!strcmp(algorithm,"fifo")) {
+		// implementar fifo
 		scan_program(virtmem,npages*PAGE_SIZE);
 
-	} else if(!strcmp(program,"focus")) {
+} else if(!strcmp(algorithm,"custom")) {
+		//implementar custom
 		focus_program(virtmem,npages*PAGE_SIZE);
 
-	} else {
-		fprintf(stderr,"unknown program: %s\n",argv[3]);
+} else {
+		fprintf(stderr,"unknown algorithm: %s\n",argv[3]);
 
 	}
+}
+
+
+
+
+
+
+if(!strcmp(program,"sort")) {
+		sort_program(virtmem,npages*PAGE_SIZE);
+
+}else if(!strcmp(program,"scan")) {
+		scan_program(virtmem,npages*PAGE_SIZE);
+
+} else if(!strcmp(program,"focus")) {
+		focus_program(virtmem,npages*PAGE_SIZE);
+
+} else {
+		fprintf(stderr,"unknown program: %s\n",argv[4]);
+
+	}
+	
+
+
+	
 
 	page_table_delete(pt);
 	disk_close(disk);
